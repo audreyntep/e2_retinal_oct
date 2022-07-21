94b5f7d7cd8c35d6ab782494ab35e71495257ef2
Audrey Ntep <audrey.ntep@hotmail.fr>
2022-07-21 10:48:13 +0200

    fix:index.js updated

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from api import create_app as create_app_api
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
 
+# modifie le type MIME des scripts js (remplace "text/plain")
+import mimetypes
+mimetypes.add_type('text/javascript', '.js')
+
+
 app_web = create_app_website()
 app_api = create_app_api()
 
@@ -12,6 +17,7 @@ apps = DispatcherMiddleware(app_web, {'/api': app_api})
 
 # Lancement de l'application
 if __name__ == '__main__':
+    
     # clear image file directories
     for root, dirs, files in os.walk("api/data"):
         for file in files:
@@ -19,5 +25,6 @@ if __name__ == '__main__':
     for root, dirs, files in os.walk("web/static/oct_image"):
         for file in files:
             os.remove(os.path.join(root, file))
+
     run_simple('127.0.0.1', 5000, apps, use_reloader=True, use_debugger=True, use_evalex=True, threaded=True)
     
--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,22 +1,22 @@
-const predictBtn = document.getElementById("predict");
-const newPredBtn = document.getElementById("signUp");
-const fistForm = document.getElementById("form1");
-const secondForm = document.getElementById("form2");
-const container = document.querySelector(".container");
-/*
-predictBtn.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
-	fistForm.submit()
+// Script entier en mode strict
+"use strict";
+
+const upload_btn = document.getElementById('btn_upload')
+const oct_image = !!document.getElementById('oct_image')
+
+const diagnose_btn = document.getElementById('btn_diagnose')
+const oct_image_diagnose = !!document.getElementById('oct_image_diagnose')
+let col_right = document.getElementById('diagnose')
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	if(oct_image){
+		browser.classList.add('hidden');
+		upload_btn.innerHTML='Change OCT image'
+	}
+	if(oct_image_diagnose){
+		diagnose_btn.classList.add('hidden');
+		col_right.classList.remove('regular-grid');
+	}
+
 });
-
-newPredBtn.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
-});
-*/
-
-//fistForm.addEventListener("submit", (e) => e.preventDefault());
-//secondForm.addEventListener("submit", (e) => e.preventDefault());
-
-
-
-
